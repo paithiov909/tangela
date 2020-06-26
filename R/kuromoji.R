559b236f4a14d9c90bce16a411262d00cc51e39e
Kato Akiru <33294906+paithiov909@users.noreply.github.com>
2020-06-26 23:47:01 +0900

    v0.0.2 (#2)

--- R/kuromoji.R
+++ R/kuromoji.R
@@ -1,29 +1,25 @@
 #' Call Kuromoji Tokenizer
 #'
 #' @param str String to be tokenized.
-#' @param userDic String
 #' @return a list
 #'
 #' @import rJava
 #' @importFrom stringi stri_enc_toutf8
 #' @export
-kuromoji <- function(str, userDic = "") {
-    Builder <- rJava::J("org.atilika.kuromoji.Tokenizer")$builder()
-    Builder$userDictionary(userDic)
-    Tokenizer <- Builder$build()
-    tokens <- rJava::.jcall(Tokenizer, "Ljava/util/List;", "tokenize", c(stringi::stri_enc_toutf8(str)))
-    res <- lapply(tokens, function(token) {
-        surface <- token$getSurfaceForm()
-        feature <- token$getAllFeatures()
-        Encoding(surface) <- "UTF-8"
-        Encoding(feature) <- "UTF-8"
-        return(list(
-            surface = surface,
-            feature = feature,
-            is_know =  token$isKnown(),
-            is_unk = token$isUnknown(),
-            is_user = token$isUser()
-        ))
-    })
-    return(res)
+kuromoji <- function(str) {
+  tokens <- rJava::.jcall(Tokenizer, "Ljava/util/List;", "tokenize", c(stringi::stri_enc_toutf8(str)))
+  res <- lapply(tokens, function(token) {
+    surface <- token$getSurfaceForm()
+    feature <- token$getAllFeatures()
+    Encoding(surface) <- "UTF-8"
+    Encoding(feature) <- "UTF-8"
+    return(list(
+      surface = surface,
+      feature = feature,
+      is_know = token$isKnown(),
+      is_unk = token$isUnknown(),
+      is_user = token$isUser()
+    ))
+  })
+  return(res)
 }
