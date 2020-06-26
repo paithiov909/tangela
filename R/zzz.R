89649d1c6dd875ea16135ba398f920d2e9aeafe6
Kato Akiru <am11.44@gmail.com>
2020-06-26 23:45:15 +0900

    v0.0.2

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,32 @@
+#' Tokenizer
+#' @noRd
+Tokenizer <- NULL
+
 #' onLoad
 #' @noRd
 #' @param libname libname
 #' @param pkgname pkgname
 #' @import rJava
 .onLoad <- function(libname, pkgname) {
-    rJava::.jpackage(pkgname, lib.loc = libname)
+  rJava::.jpackage(pkgname, lib.loc = libname)
 
-    # For dev environment
-    rJava::.jaddClassPath("inst/java/kuromoji-0.7.7.jar")
+  # For dev environment
+  rJava::.jaddClassPath("inst/java/kuromoji-0.7.7.jar")
 
-    rJava::javaImport(packages = "org.atilika.kuromoji.Token")
-    rJava::javaImport(packages = "org.atilika.kuromoji.Tokenizer")
+  rJava::javaImport(packages = "org.atilika.kuromoji.Token")
+  rJava::javaImport(packages = "org.atilika.kuromoji.Tokenizer")
+
+  # Initialize
+  user_dic <- ""
+
+  Builder <- rJava::J("org.atilika.kuromoji.Tokenizer")$builder()
+  Builder$userDictionary(user_dic)
+  Tokenizer <<- Builder$build()
 }
 
 #' onUnload
 #' @noRd
 #' @param libpath libpath
 .onUnload <- function(libpath) {
-    #### unload ####
+  # No use...
 }
